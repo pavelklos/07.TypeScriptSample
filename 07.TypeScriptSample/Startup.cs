8a411eceb4c0775451969b62428aafaf5fd1ac1e
pavelklos <pavelkloscz@gmail.com>
2018-10-17 05:46:43 +0200

    First commit and first TypeScript file ICustomer.ts

--- 07.TypeScriptSample/Startup.cs
+++ 07.TypeScriptSample/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace _07.TypeScriptSample
 {
@@ -18,13 +19,19 @@ namespace _07.TypeScriptSample
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            loggerFactory.AddConsole();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStaticFiles();
+            app.UseDefaultFiles();
+
+            // DELETE
             app.Run(async (context) =>
             {
                 await context.Response.WriteAsync("Hello World!");
---
+++ 07.TypeScriptSample/wwwroot/js/ICustomer.js
@@ -0,0 +1,10 @@
+System.register([], function (exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    return {
+        setters: [],
+        execute: function () {
+        }
+    };
+});
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiSUN1c3RvbWVyLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiLi4vLi4vc3JjL0lDdXN0b21lci50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7Ozs7UUFLQSxDQUFDIiwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGludGVyZmFjZSBJQ3VzdG9tZXIge1xyXG4gICAgbmFtZTogc3RyaW5nO1xyXG4gICAgbnVtYmVyT2ZZZWFyc0N1c3RvbWVyOiBudW1iZXI7XHJcbiAgICBkaXNjb3VudFBlcmNlbnQoKTogbnVtYmVyOyAvLyBtZXRob2QgcmV0dXJucyBudW1iZXJcclxufVxyXG4iXX0=
